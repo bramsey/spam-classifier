e6eccbd33895478b69ba81076191afdd66b479dd
Bill Ramsey <bill@billiamram.com>
2012-08-21 17:34:50 -0400

    added spammicity calculation

--- spam.rb
+++ spam.rb
@@ -5,15 +5,17 @@ Dir.chdir File.dirname(__FILE__)
 }
 
 @label_totals = {
-    :spam => 0,
-    :ham => 0
+    :spam => 0.0,
+    :ham => 0.0
 }
 
+@word_spammicity = {}
+
 @spam_hash = {}
-@spam_count = 0
+@spam_count = 0.0
 
 @ham_hahs = {}
-@ham_count = 0
+@ham_count = 0.0
 
 def load_files(dir)
     files = []
@@ -41,6 +43,42 @@ def train(email, label)
     @label_totals[label] += 1
 end
 
+def calc_spammicity(word)
+    spam_count = @word_counts[:spam][word] || 0.0
+    ham_count = @word_counts[:ham][word] || 0.0
+    spam_total = @label_totals[:spam]
+    ham_total = @label_totals[:ham]
+
+
+    unless spam_count + ham_count < 5
+        p_spam = [1.0, spam_count / spam_total].min
+        p_ham = [1.0, ham_count / ham_total].min
+
+        if (p_spam + p_ham) > 0
+            spammicity = 
+                [0.01,
+                    [0.99,
+                        p_spam / (p_spam + p_ham) 
+                    ].min
+                ].max
+        else
+            puts "#{p_spam} | #{p_ham}"
+            spammicity = 0.5
+        end
+    else
+        spammicity = 0.5
+    end
+end
+
+def build_spammicity_hash 
+    @word_counts[:spam].each do |word, count|
+        @word_spammicity[word] ||= calc_spammicity(word)
+    end
+    @word_counts[:ham].each do |word, count|
+        @word_spammicity[word] ||= calc_spammicity(word)
+    end
+end
+
 def classify(email)
 
 end
@@ -53,5 +91,4 @@ end
 ham_files.each do |file|
     train file, :ham
 end
-
-puts @word_counts[:ham]
+build_spammicity_hash
